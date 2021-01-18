45ed540815893f38c48c90927fc38b4f2a104443
GBelonozhko <44050866+GBelonozhko@users.noreply.github.com>
2021-01-18 13:08:26 -0800

    m

--- server.js
+++ server.js
@@ -74,12 +74,12 @@ app.use("/api", todolistRoutes);
 app.use("/api", blog);
 
 
-app.use(express.static(path.join(__dirname, 'build')));
+//app.use(express.static(path.join(__dirname, 'build')));
 
 
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+//app.get('*', function(req, res) {
+//    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//});
 
 
 
